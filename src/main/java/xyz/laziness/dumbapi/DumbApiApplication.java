27800ea0b7db90f1822bb4317a6f8a4a01a2ad6d
hyunwoo.shim <shim.hw0810@gmail.com>
2018-03-11 00:55:48 +0900

    init Project

---
+++ src/main/java/xyz/laziness/dumbapi/DumbApiApplication.java
@@ -0,0 +1,12 @@
+package xyz.laziness.dumbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DumbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DumbApiApplication.class, args);
+	}
+}
---
+++ src/test/java/xyz/laziness/dumbapi/DumbApiApplicationTests.java
@@ -0,0 +1,16 @@
+package xyz.laziness.dumbapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DumbApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
